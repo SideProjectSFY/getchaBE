1c7aeddf7ecf141aa096a1612c9323d4a2424f03
Kim, Ji-Hea <bictiger3@gmail.com>
2025-11-23 23:38:22 +0900

    Feat : api prefix 설정

---
+++ backend/src/main/java/com/ssafy/backend/common/config/ApiPrefixConfig.java
@@ -0,0 +1,18 @@
+package com.ssafy.backend.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ApiPrefixConfig implements WebMvcConfigurer {
+
+    private static final String API_PREFIX = "/api/v1";
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix(API_PREFIX, clazz ->
+                clazz.isAnnotationPresent(RestController.class));
+    }
+}
